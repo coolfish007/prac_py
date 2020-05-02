a68e6f4e9262377ca204113a26b64b08374522e7
coolfish007 <liuhongchen@idatagz.com>
2020-05-02 23:14:50 +0800

    使用apply对每行数据进行处理,并新增列以及多列

--- pydata-exercise/panas练习debug.py
+++ pydata-exercise/panas练习debug.py
@@ -36,3 +36,76 @@ def testdfgo(df):
 df = pd.DataFrame([[0, 0], [5, 6]], columns=['A', 'B'])
 testdfgo(df)
 print(df)
+
+# %% [markdown]
+# ## datafrmae apply,每行的数据处理后并新增列
+# %%
+data = {
+    'state': ['Ohio', 'Ohio', 'Ohio', 'Nevada', 'Nevada', 'Nevada'],
+    'year': [2000, 2001, 2002, 2001, 2002, 2003],
+    'pop': [1.5, 1.7, 3.6, 2.4, 2.9, 3.2]
+}
+frame = pd.DataFrame(data)
+
+# for row in frame.itertuples():
+#    print(row)
+
+
+def new_value0(row):
+    print(row)
+    return (5, 5)
+
+
+def new_value(x=0, y=0):
+    x = x + 5
+    y = y * 5
+    return x
+
+
+def new_value1(row):
+    print(row)
+    row['new_year'] = row['year'] + 5
+    row['new_pop'] = row['pop'] * 5
+    return row
+
+
+def new_value2(row, year, pop):
+    x = row[year] + 5
+    y = row[pop] * 5
+    return x, y
+
+
+# %%
+frame['new_year'] = frame.apply(lambda row: row['year'] + 5, axis=1)
+frame['new_year'] = frame.apply(lambda row: new_value(row['year']), axis=1)
+
+# %%
+frame = frame.apply(new_value1, axis=1)
+print(frame)
+
+# %%
+frame['new_year'], frame['new_pop'] = zip(
+    *frame[['year', 'pop']].apply(new_value0, axis=1))
+print(frame)
+# %%
+frame['new_year'], frame['new_pop'] = zip(
+    *frame[['year', 'pop']].apply(new_value2, axis=1, args=('year', 'pop')))
+frame['new_year'], frame['new_pop'] = zip(
+    *frame[['year', 'pop']].apply(new_value2, axis=1, year='year', pop='pop'))
+print(frame)
+
+# %%
+x1 = '5,6,7'
+y1 = '1,2,3'
+x1 = map(int, x1.split(','))
+y1 = map(int, y1.split(','))
+la = list(zip(x1, y1))
+lb = []
+
+for e in la:
+    lb.append((e[0]+1, e[1]+1))
+
+print(lb)
+lb = list(zip(*lb))
+print(lb)
+# %%
