9329cb29e6baa1119b9d3d0df493f9ca465d7d28
coolfish007 <liuhongchen@idatagz.com>
2020-05-04 12:52:03 +0800

    gcj02->wgs84,转换公交线路中的坐标,以及公交车站的坐标.

--- 爬取公交线路与车站.py
+++ 爬取公交线路与车站.py
@@ -25,6 +25,7 @@
 #  ### 高德地图爬取的共用内容
 
 # %%
+import math
 import requests
 import xlwt
 import pandas as pd
@@ -131,8 +132,109 @@ def getonebusline(busline_df, station_df, busline_name):
 
 
 # %% [markdown]
+#
+
+# %%
+pi = 3.1415926535897932384626  # π
+a = 6378245.0  # 长半轴
+ee = 0.00669342162296594323  # 扁率
+
+
+def out_of_china(lng, lat):
+    """
+    判断是否在国内，不在国内不做偏移
+    :param lng:
+    :param lat:
+    :return:
+    """
+    return not (lng > 73.66 and lng < 135.05 and lat > 3.86 and lat < 53.55)
+
+
+def _transformlat(lng, lat):
+    ret = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * lat + \
+        0.1 * lng * lat + 0.2 * math.sqrt(math.fabs(lng))
+    ret += (20.0 * math.sin(6.0 * lng * pi) + 20.0 *
+            math.sin(2.0 * lng * pi)) * 2.0 / 3.0
+    ret += (20.0 * math.sin(lat * pi) + 40.0 *
+            math.sin(lat / 3.0 * pi)) * 2.0 / 3.0
+    ret += (160.0 * math.sin(lat / 12.0 * pi) + 320 *
+            math.sin(lat * pi / 30.0)) * 2.0 / 3.0
+    return ret
+
+
+def _transformlng(lng, lat):
+    ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + \
+        0.1 * lng * lat + 0.1 * math.sqrt(math.fabs(lng))
+    ret += (20.0 * math.sin(6.0 * lng * pi) + 20.0 *
+            math.sin(2.0 * lng * pi)) * 2.0 / 3.0
+    ret += (20.0 * math.sin(lng * pi) + 40.0 *
+            math.sin(lng / 3.0 * pi)) * 2.0 / 3.0
+    ret += (150.0 * math.sin(lng / 12.0 * pi) + 300.0 *
+            math.sin(lng / 30.0 * pi)) * 2.0 / 3.0
+    return ret
+
+
+def gcj02_to_wgs84(lng, lat):
+    """
+    GCJ02(火星坐标系)转GPS84
+    :param lng:火星坐标系的经度
+    :param lat:火星坐标系纬度
+    :return:
+    """
+    if out_of_china(lng, lat):
+        return lng, lat
+    dlat = _transformlat(lng - 105.0, lat - 35.0)
+    dlng = _transformlng(lng - 105.0, lat - 35.0)
+    radlat = lat / 180.0 * pi
+    magic = math.sin(radlat)
+    magic = 1 - ee * magic * magic
+    sqrtmagic = math.sqrt(magic)
+    dlat = (dlat * 180.0) / ((a * (1 - ee)) / (magic * sqrtmagic) * pi)
+    dlng = (dlng * 180.0) / (a / sqrtmagic * math.cos(radlat) * pi)
+    mglat = lat + dlat
+    mglng = lng + dlng
+    return [lng * 2 - mglng, lat * 2 - mglat]
+
+
+# %% [markdown]
 #  ### getonebusline-方式2
 # %%
+def change_gcj02_in_busline(row, xs, ys):
+    xs_value = row[xs]
+    ys_value = row[ys]
+    gcj02_lst = list(zip(map(float, xs_value.split(',')), map(float, ys_value.split(','))))
+    wgs84_lst = []
+    for coords in gcj02_lst:
+        result = gcj02_to_wgs84(coords[0], coords[1])
+        lng = str(round(result[0], 6))
+        lat = str(round(result[1], 6))
+        wgs84_lst.append((lng, lat))
+    wgs84_lst = list(zip(*wgs84_lst))
+    return ','.join(wgs84_lst[0]), ','.join(wgs84_lst[1])
+
+
+def change_gcj02_in_station(row, xy_coords, poiid2_xy):
+    xy_coords_value = list(map(float, row[xy_coords].split(';')))
+    poiid2_xy_value = list(map(float, row[poiid2_xy].split(';')))
+    result = gcj02_to_wgs84(xy_coords_value[0], xy_coords_value[1])
+    xy_coords_84_lst = map(lambda v: str(round(v, 6)), result)
+    xy_coords_84_str = ';'.join(xy_coords_84_lst)
+    result = gcj02_to_wgs84(poiid2_xy_value[0], poiid2_xy_value[1])
+    poiid2_xy_84_lst = map(lambda v: str(round(v, 6)), result)
+    poiid2_xy_84_str = ';'.join(poiid2_xy_84_lst)
+    return xy_coords_84_str, poiid2_xy_84_str
+
+
+def change_gcj02_in_station2(row, xy_coords, poiid2_xy):
+    xy_coords_value = list(map(float, row[xy_coords].split(';')))
+    poiid2_xy_value = list(map(float, row[poiid2_xy].split(';')))
+    result = gcj02_to_wgs84(xy_coords_value[0], xy_coords_value[1])
+    xy_coords_84 = result
+    result = gcj02_to_wgs84(poiid2_xy_value[0], poiid2_xy_value[1])
+    poiid2_xy_84 = result
+    return xy_coords_84, poiid2_xy_84
+
+
 def getonebusline2(busline_name):
     busline_url = 'https://ditu.amap.com/service/poiInfo?keywords=' + busline_name
     response = requests.get(busline_url, headers=headers, params=params)
@@ -140,25 +242,29 @@ def getonebusline2(busline_name):
     busline = pd.DataFrame()
     oneline_stations = pd.DataFrame()
     if data['data']['message'] and data['data']['busline_list']:
-        print('地图查询找到公交线路:[%s]' % busline_name)
+        print('当前公交线路:[%s],地图已查询到.' % busline_name)
         busline_dict = data['data']['busline_list']
         if (len(busline_dict) >= 2):
-            print('抽取公交线路(双向):[%s]的详情' % busline_name)
+            print('--抽取公交线路(双向):[%s]的详情' % busline_name)
             busline = pd.DataFrame(busline_dict, columns=busline_df_column)
             busline.iloc[0]['type'] = 'a2b'
             busline.iloc[1]['type'] = 'b2a'
             busline = busline[:2]
+            busline['xs-84'], busline['ys-84'] = zip(
+                *busline[['xs', 'ys']].apply(change_gcj02_in_busline, axis=1, xs='xs', ys='ys'))
             i = 0
             while (i < 2):
                 tmp = getbusstations(busline_dict[i], 'stations')
-                print('抽取公交线路:[%s-%s]的站点,此线路共有[%s]个站点' %
-                      (busline_name, busline.iloc[i]['type'], len(tmp)))
+                print('--抽取公交线路:[%s-%s]的站点,此线路共有[%d]个站点' % (busline_name, busline.iloc[i]['type'], len(tmp)))
+                print(','.join(tmp['name']))
                 tmp['busline_id'] = busline.iloc[i]['id']
                 tmp['order'] = tmp.index
+                tmp['xy_coords_84'], tmp['poiid2_xy_84'] = zip(
+                    *tmp[['xy_coords', 'poiid2_xy']].apply(change_gcj02_in_station, axis=1, args=('xy_coords', 'poiid2_xy')))
                 oneline_stations = oneline_stations.append(tmp, ignore_index=True)
                 i += 1
     else:
-        print('地图---没有---查询找到公交线路:[%s]' % busline_name)
+        print('$$没有查询到公交线路:[%s]' % busline_name)
     return busline, oneline_stations
 
 
@@ -202,12 +308,10 @@ for busline_name in buslines.split(','):
     else:
         busline_df = busline_df.append(busline, ignore_index=True)
         station_df = station_df.append(stations, ignore_index=True)
-print('共有双向线路: [%s] 条' % len(busline_df))
-print(busline_df)
-print('共有站点(双向): [%s] 个' % len(station_df))
 print(station_df)
-print('此次查询 [%d] 条线路的信息' % len(buslines.split(',')))
-print('此次查询共有: [%d] 条线路没有详情, 其线路具体内容:' % len(line_noinfo))
+print(busline_df)
+print('数据汇总:查询[%d]条线路,抽取到详情[%d]条线路,涉及站点: [%s] 个' % (len(buslines.split(',')), len(busline_df)/2, len(station_df)/2))
+print('共有: [%d] 条线路--没有详情--, 其线路名称:' % len(line_noinfo))
 print(','.join(i for i in line_noinfo))
 
 
