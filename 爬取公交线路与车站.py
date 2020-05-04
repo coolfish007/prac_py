034d974c22dbfdacd1241c605126d00f434cedbb
coolfish007 <liuhongchen@idatagz.com>
2020-05-05 01:14:35 +0800

    1.多次运行不断追加数据;2.把测试代码独立出来形成爬虫使用的测试代码.

--- 爬取公交线路与车站.py
+++ 爬取公交线路与车站.py
@@ -36,16 +36,17 @@ from openpyxl import load_workbook
 headers = {
     'authority': 'ditu.amap.com',
     'accept': '*/*',
-    'x-csrf-token': '611e34356f4e75a6b4fe490b72c2c7a6',
+    'x-csrf-token': 'b20895a13b20f5e8e4b4a66ee5006bda',
     'x-requested-with': 'XMLHttpRequest',
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36',
     'amapuuid': '1440c18d-8262-4b10-bf1d-dcfa90af7406',
     'sec-fetch-site': 'same-origin',
     'sec-fetch-mode': 'cors',
     'sec-fetch-dest': 'empty',
-    'referer': 'https://ditu.amap.com/search?query=%E7%8E%AF%E8%B7%AF&city=210100&geoobj=123.395895%7C41.775079%7C123.459137%7C41.806199&zoom=14.12',
+    'referer': 'https://ditu.amap.com/search?query=%E7%8E%AF%E8%B7%AF&city=210100&geoobj=123.235132%7C41.790515%7C123.583626%7C41.961776&zoom=11.66',
     'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
-    'cookie': 'UM_distinctid=1714efec5d91ad-0dcddda99ab246-396a7f06-fa000-1714efec5da7ca; cna=BEnxFtRzAkoCAXFCoBgfzgXk; CNZZDATA1255827602=412918052-1586405683-https%253A%252F%252Flbs.amap.com%252F%7C1586418799; _ga=GA1.2.1633570714.1587224095; passport_login=MzE2OTE2MSxhbWFwNW9mbjlkRVUsMnhsNzQzdXFqN2kzdmpyeXR6NWxjcWZieWd4MmZzemYsMTU4NzYxMTcxMixaVEJsTWpSa016UTBZelJqTldKall6WmtPVEJpWmpFeU4ySmtaR0V5TjJRPQ%3D%3D; oauth_state=ae8fa31ae280a2a0e644142c408b11cd; dev_help=Cm5hSMAkvpmaraCOySAMG2UwNDMyNTczMWE0OGI5ODIwNzIwYmViN2NjNmQ0NDE4N2E1NzZjMTUwMmRhNDlhMTQ3MzMwZjVhZmE4YTc5NTEus7mSrdBi1mHDTTh7gNwboXIGicOxRQcgdcUXFEULilQ2nc8zq%2BQ9cZkm1Nb3j0GTZnjb7yMdphosNOkRy%2F6ikMiSMXzZNCnLuZnVvfNkX4LBtvXzfH9D8hHbzRUHWbs%3D; CNZZDATA1255626299=500121251-1586274166-https%253A%252F%252Flbs.amap.com%252F%7C1588249307; x-csrf-token=611e34356f4e75a6b4fe490b72c2c7a6; x5sec=7b22617365727665723b32223a223130633936393836383138333530326433646637303737313762623134353238434e6a4e752f5546454c50526c4976346a6f624a52673d3d227d; l=eBPMb8PRQmIP7Vk9BO5wPurza77OrHAjnsPzaNbMiIHcIsin5F4kDcgjMJomK4yA7HG5-tCXy9xP8XiKbRev-QaNwtrsywzdDt9xUxJO.; isg=BO3sjG0rT-L9wSus6PvLWfmW_I9nSiEcaM2O-S_kaQSzpltYlZse7Bg0lHpAJjnU',
+    'cookie': 'UM_distinctid=1714efec5d91ad-0dcddda99ab246-396a7f06-fa000-1714efec5da7ca; cna=BEnxFtRzAkoCAXFCoBgfzgXk; CNZZDATA1255827602=412918052-1586405683-https%253A%252F%252Flbs.amap.com%252F%7C1586418799; _ga=GA1.2.1633570714.1587224095; passport_login=MzE2OTE2MSxhbWFwNW9mbjlkRVUsMnhsNzQzdXFqN2kzdmpyeXR6NWxjcWZieWd4MmZzemYsMTU4NzYxMTcxMixaVEJsTWpSa016UTBZelJqTldKall6WmtPVEJpWmpFeU4ySmtaR0V5TjJRPQ%3D%3D; oauth_state=ae8fa31ae280a2a0e644142c408b11cd; dev_help=Cm5hSMAkvpmaraCOySAMG2UwNDMyNTczMWE0OGI5ODIwNzIwYmViN2NjNmQ0NDE4N2E1NzZjMTUwMmRhNDlhMTQ3MzMwZjVhZmE4YTc5NTEus7mSrdBi1mHDTTh7gNwboXIGicOxRQcgdcUXFEULilQ2nc8zq%2BQ9cZkm1Nb3j0GTZnjb7yMdphosNOkRy%2F6ikMiSMXzZNCnLuZnVvfNkX4LBtvXzfH9D8hHbzRUHWbs%3D; guid=73b2-4fdc-b9ff-d417; x-csrf-token=b20895a13b20f5e8e4b4a66ee5006bda; CNZZDATA1255626299=500121251-1586274166-https%253A%252F%252Flbs.amap.com%252F%7C1588604569; l=eBPMb8PRQmIP7Q_bBO5Zhurza7793IdfcsPzaNbMiIHca1zPJFMJpNQcksVW6dtjgtCj5h-P8XiKbR3DJIzNwtrsywzdDt9x3xvR.; isg=BDc3615dJb7myKEODk0RW8eoxi2B_AteJmOE24nlwYZtOF96hsxXrtFaHphm0OPW',
+    'if-none-match': 'W/"1b6f7-SoSjGUlTkPIxfxEeqavxip5gXtY"',
 }
 
 params = (
@@ -64,6 +65,7 @@ params = (
     ('geoobj', '123.395895|41.775079|123.459137|41.806199'),
 )
 
+
 # %% [markdown]
 #  ### getbusstations
 
@@ -78,63 +80,8 @@ def getbusstations(busline_dict, stations_key):
 
 
 # %% [markdown]
-#  ### getonebusline-测试代码
-#  测试用代码，取出一条线路具体内容并组装成dataframe。
-# %%
-busline_name = '环路'
-busline_url = 'https://ditu.amap.com/service/poiInfo?keywords=' + busline_name
-response = requests.get(busline_url, headers=headers, params=params)
-data = response.json()
-busline_df_column = [
-    'id', 'code', 'type', 'key_name', 'name', 'front_name', 'terminal_name',
-    'company', 'status', 'ic_card', 'air', 'total_price', 'start_time',
-    'end_time', 'interval', 'xs', 'ys'
-]
-busline_df = pd.DataFrame(columns=busline_df_column)
-busline_dict = data['data']['busline_list'][0]
-sub0 = {
-    key: busline_dict[key]
-    for key in busline_df_column & busline_dict.keys()
-}
-busline_df = busline_df.append(sub0, ignore_index=True)
-print(busline_df)
-
-# %% [markdown]
-#  ### getonebusline-方式1
-#  重新定义getonebusline最好不要把dataframe当参数传递进去再返回.直接返回一条线路相关的内容在调用外边append会好一些.
-#  函数说明：
-#  1）每条线路的取正反向两条线路；
-#  2）每条线路用高德地图busline唯一的id做关联
-#  3）注意station有'poiid1','xy_coords','poiid2','poiid2_xy'，实际上同名称公交站点可能有多个，地理位置上离的也不远，不同线路的使用的站也不一样，poiid1是此线路中本站的实际具体位置。其中poiid2是某个名称的公交车站在地图上查找时返回的站点地址，可以理解成此名称的公交车站的代表，从高德poiid查找位置的api可以验证。
-
-
-# %%
-def getonebusline(busline_df, station_df, busline_name):
-    busline_url = 'https://ditu.amap.com/service/poiInfo?keywords=' + busline_name
-    response = requests.get(busline_url, headers=headers, params=params)
-    data = response.json()
-    if data['data']['message'] and data['data']['busline_list']:
-        print('找到公交线路:%s' % busline_name)
-        busline_dict = data['data']['busline_list']
-        if (len(busline_dict) >= 2):
-            print('开始抽取公交线路:%s的内容' % busline_name)
-            tmp = pd.DataFrame(busline_dict, columns=busline_df_column)
-            tmp.iloc[0]['type'] = 'a2b'
-            tmp.iloc[1]['type'] = 'b2a'
-            busline_df = busline_df.append(tmp[:2], ignore_index=True)
-            i = 0
-            while (i < 2):
-                tmp = getbusstations(busline_dict[i], 'stations')
-                tmp['busline_id'] = busline_df.iloc[i - 2]['id']
-                tmp['order'] = tmp.index
-                print('抽取公交线路:%s的站点,此线路共有%s个站点' % (busline_name, len(tmp)))
-                station_df = station_df.append(tmp, ignore_index=True)
-                i += 1
-    return busline_df, station_df
-
-
-# %% [markdown]
-# ### 坐标转换的代码,todo:放在独立的文件中
+# ### 坐标转换的代码.
+# TODO: 1.此函数放放在独立的包中.
 
 # %%
 pi = 3.1415926535897932384626  # π
@@ -249,9 +196,10 @@ def getonebusline2(busline_name):
         if (len(busline_dict) >= 2):
             print('--抽取公交线路(双向):[%s]的详情' % busline_name)
             busline = pd.DataFrame(busline_dict, columns=busline_df_column)
-            busline.iloc[0]['type'] = 'a2b'
-            busline.iloc[1]['type'] = 'b2a'
+            busline.loc[0, 'type'] = 'a2b'
+            busline.loc[1, 'type'] = 'b2a'
             busline = busline[:2]
+            busline['search_name'] = busline_name
             busline['xs-84'], busline['ys-84'] = zip(
                 *busline[['xs', 'ys']].apply(change_gcj02_in_busline, axis=1, xs='xs', ys='ys'))
             i = 0
@@ -276,13 +224,13 @@ def getonebusline2(busline_name):
 # buslines = '环路,100,101'
 buslines = '环路,苦茶,100,101'
 busline_df_column = [
-    'id', 'code', 'type', 'key_name', 'name', 'front_name', 'terminal_name',
+    'id', 'code', 'type', 'search_name', 'key_name', 'name', 'front_name', 'terminal_name',
     'company', 'status', 'ic_card', 'air', 'total_price', 'start_time',
-    'end_time', 'interval', 'xs', 'ys'
+    'end_time', 'interval', 'xs', 'ys', 'xs-84', 'ys-84'
 ]
 station_df_column = [
-    'name', 'busline_id', 'order', 'poiid1', 'xy_coords', 'poiid2',
-    'poiid2_xy', 'station_id'
+    'station_id', 'name', 'busline_id', 'order', 'poiid1', 'xy_coords', 'xy_coords_84', 'poiid2',
+    'poiid2_xy', 'poiid2_xy_84'
 ]
 busline_df = pd.DataFrame(columns=busline_df_column)
 station_df = pd.DataFrame(columns=station_df_column)
@@ -295,40 +243,52 @@ line_noinfo = []
 # %%
 f_name = 'data/hgj_gong_jiao_xian_lu.xlsx'
 book = load_workbook(f_name)
-busline_from_excel = pd.read_excel(f_name, sheet_name='hgj_gong_jiao_xian_lu')
 writer = pd.ExcelWriter(f_name, engine='openpyxl')
 writer.book = book
 writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+
+busline_sheet_name = 'busline_info'
+stations_sheet_name = 'stations_info'
+busline_from_excel = pd.read_excel(f_name, sheet_name='hgj_gong_jiao_xian_lu')
+oldbusline = pd.DataFrame()
+oldstations = pd.DataFrame()
+if(busline_sheet_name in writer.sheets):
+    oldbusline = pd.read_excel(f_name, sheet_name=busline_sheet_name)
+    oldstations = pd.read_excel(f_name, sheet_name=stations_sheet_name)
 
 busline_lst = []
 for bn in busline_from_excel['xian_lu_ming_cheng']:
     busline_lst.append(bn)
 buslines = ','.join(busline_lst)
 print(buslines)
-# buslines = '环路'
 # %% [markdown]
-#  ### getonebusline-调用1
-
+# 运行多次不断追加原excel文件的内容写入
 # %%
-for busline_name in buslines.split(','):
-    busline_df, station_df = getonebusline(busline_df, station_df,
-                                           busline_name)
-print('共有双向线路:%s条' % len(busline_df))
-print(busline_df)
-print('共有站点:%s个' % len(station_df))
-print(station_df)
+def write2excel(oldbusline, newbusline, oldstations, newstations):
+    allbusline = oldbusline.append(newbusline, ignore_index=True)
+    allstations = oldstations.append(newstations, ignore_index=True)
+    allbusline.to_excel(writer, sheet_name=busline_sheet_name, header=True)
+    allstations.to_excel(writer, sheet_name=stations_sheet_name, header=True)
+    writer.save()
+    writer.close()
+
 
 # %% [markdown]
 #  ### getonebusline-调用2
+#  TODO: 1.将没有线路详情的线路输出到文件中,如果是停运的话做标识;2.标识a2b和b2a中途径站不同的线路.
 # %%
 try:
+    exist_busline_lst = list(oldbusline['search_name'])
     for busline_name in buslines.split(','):
-        busline, stations = getonebusline2(busline_name)
-        if (len(busline) == 0):
-            line_noinfo.append(busline_name)
+        if (busline_name in exist_busline_lst):
+            print('--当前公交线路:[%s] 已存在--' % busline_name)
         else:
-            busline_df = busline_df.append(busline, ignore_index=True)
-            station_df = station_df.append(stations, ignore_index=True)
+            busline, stations = getonebusline2(busline_name)
+            if (len(busline) == 0):
+                line_noinfo.append(busline_name)
+            else:
+                busline_df = busline_df.append(busline, ignore_index=True)
+                station_df = station_df.append(stations, ignore_index=True)
         time.sleep(random.random() * random.randint(0, 7) + random.randint(0, 5))
 except KeyError:
     print('--高德限制IP错误,以下是本次获取到的数据汇总:--')
@@ -341,11 +301,7 @@ if(len(line_noinfo) > 0):
     print('共有: [%d] 条线路--没有详情--, 其线路名称:' % len(line_noinfo))
     print(','.join(line_noinfo))
 
-if len(busline_df) > 0:
-    busline_df.to_excel(writer, sheet_name='busline_info', header=True)
-    station_df.to_excel(writer, sheet_name='stations_info', header=True)
-    writer.save()
-writer.close()
+write2excel(oldbusline, busline_df, oldstations, station_df)
 
 
 # %%
