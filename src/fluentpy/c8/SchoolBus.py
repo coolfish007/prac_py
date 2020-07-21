e0303840d720fd8450d5f55a15957e22ed6e90f7
coolfish007 <liuhongchen@idatagz.com>
2020-07-21 15:22:23 +0800

    C8 练习

---
+++ src/fluentpy/c8/SchoolBus.py
@@ -0,0 +1,12 @@
+class SchoolBus:
+    def __init__(self, passengers=None) -> None:
+        if passengers is None:
+            self.passengers = []
+        else:
+            self.passengers = list(passengers)
+
+    def pick(self, name):
+        self.passengers.append(name)
+
+    def drop(self, name):
+        self.passengers.remove(name)
